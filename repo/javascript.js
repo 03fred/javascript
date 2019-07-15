e630de4948d60a4d1bd2f2ed885f30840007bc3c
03fred <fred.rocke02@gmail.com>
2019-07-15 13:10:45 -0300

    first commit

---
+++ repo/javascript.js
@@ -0,0 +1,18 @@
+//client side
+
+//alert('Olá mundo');
+
+/*var mensagem = 'Olá Mundo';
+alert(mensagem);*/
+
+var a = 3;
+var b = 2;
+
+var soma = a + b;
+var sub = b - a;
+var multi = a * b;
+var div = b / a;
+console.log('Soma ' + soma);
+console.log('Sub ' + sub);
+console.log('multi ' + multi);
+console.log('div ' + div);
