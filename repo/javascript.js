9d0677482f712cf6245162f7ab646fa7ef867844
03fred <fred.rocke02@gmail.com>
2019-07-15 13:55:11 -0300

    js completed

--- repo/javascript.js
+++ repo/javascript.js
@@ -3,7 +3,7 @@
 //alert('Olá mundo');
 
 /*var mensagem = 'Olá Mundo';
-alert(mensagem);*/
+alert(mensagem);
 
 var a = 3;
 var b = 2;
@@ -12,7 +12,79 @@ var soma = a + b;
 var sub = b - a;
 var multi = a * b;
 var div = b / a;
+                    ESTRUTURA CONDICICIONAL
 console.log('Soma ' + soma);
 console.log('Sub ' + sub);
 console.log('multi ' + multi);
 console.log('div ' + div);
+console.log(3 % 2); //resto da divisão
+
+if (b % 2 === 0) {
+    alert('par');
+} else {
+    alert('ímpar');
+}
+
+//              ESTRUTURA REPETIÇÃO
+
+var i = 0;
+while (i < 3) {
+    alert(i);
+    i = i + 1;
+
+}
+
+//for 
+
+var j = 0;
+for (j; j < 3; j++) {
+    alert(j);
+}
+
+
+var numer = 6;
+var decimal = 5.6; //numeros flutuantes
+var string = "olá mundo";
+var lsita = ["laranja", "maçã", "banana"];
+console.log(lsita);
+
+for (i in lsita) {
+    alert(lsita[i]);
+}
+
+-----------------FUNÇÕES
+
+function soma(a, b) {
+    console.log(a + b);
+}
+
+//soma(2, 2);
+
+function subtracao(a, b) {
+    return a - b;
+}
+
+console.log(subtracao(2, 3));
+
+-------------------Eventos 
+function mensagem(nome) {
+    alert(nome + ": clickou");
+}
+------------------ Estilização js
+function mudaCor(cor) {
+    var elemento = document.getElementById("idTest");
+    // elemento.style.color = cor;
+    elemento.style.backgroundColor = cor;
+}
+------------------ Validação de formulário
+*/
+function valida() {
+
+    var nome = document.getElementById("nome").value;
+    if (nome == "") {
+        alert('nome não pode ser vazio');
+    }
+
+
+
+}
